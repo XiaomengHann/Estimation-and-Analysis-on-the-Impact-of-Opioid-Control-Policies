d727e2d40587a8e7abd2f80eb68e2d8a74239f6f
XiaomengHann <46758912+XiaomengHann@users.noreply.github.com>
2019-10-30 20:02:26 -0400

    Delete 01_import_populations.py

--- 10_code/Population/01_import_population.py
+++
@@ -1,4 +0,0 @@
-cd C:\Users\home\Desktop\class\IDS 690-02\mid-term project
-import pandas as pd
-pop1 = pd.read_csv("co-est00int-tot.csv",encoding = "ISO-8859-1")
-pop2 = pd.read_csv("co-est2018-alldata.csv",encoding = "ISO-8859-1")
