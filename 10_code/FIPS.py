64309cf348df2c47cf06e8f5ae9cfea6c3f1d23f
joehs16 <56398757+joehs16@users.noreply.github.com>
2019-11-06 17:35:36 -0500

    Update FIPS.py
    currently have 323 files that are nan in the fips list

--- 10_code/FIPS.py
+++ 10_code/FIPS.py
@@ -1,9 +1,23 @@
 import pandas as pd
+import numpy as np
 import csv
+import os
 
-df = pd.read_csv('arcos_all_washpost.tsv', delimiter = "\t", nrows = 300, encoding = 'utf-8')
+os.chdir('/Users/joseph.hsieh/Documents/GitHub/estimating-impact-of-opioid-prescription-regulations-team-1/')
+# varify the path using getcwd()
+cwd = os.getcwd()
+# print the current directory
+print("Current working directory is:", cwd)
 
-df.head(10)
-df.BUYER_NAME.head()
-for i in df:
-    print(i)
+#import datasets
+arcos_all = pd.read_csv('20_intermediate_files/arcos_intermediate/arcos_grouped_all.csv')
+FIPS_list = pd.read_csv('00_source/FIPS_list.csv')
+
+#clean FIPS dataset
+FIPS_list.rename(columns={"Name":"COUNTY","State":"STATE"},inplace=True)
+FIPS_list['COUNTY'] = FIPS_list['COUNTY'].str.upper()
+
+arcos_all
+FIPS_list
+arcos_merged =  pd.merge(arcos_all, FIPS_list, on=['COUNTY','STATE'], how='left')
+arcos_merged[arcos_merged['FIPS'].isna()]
