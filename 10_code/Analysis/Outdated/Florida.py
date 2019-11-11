36e785c60ec8faf57934913942b09915e9945f8e
joaomansur <joaoalbertomansur@gmail.com>
2019-11-11 18:20:14 -0500

    Analysis in one file, rough draft done

--- 10_code/Analysis/Outdated/Florida.py
+++ 10_code/Analysis/Outdated/Florida.py
@@ -11,13 +11,15 @@ prescriptions = pd.read_csv("../../20_intermediate_files/Prescriptions_Intermedi
 #Merge prescriptions
 
 prescriptions = prescriptions.drop(columns = ['Unnamed: 0','Unnamed: 0.1']) 
+prescriptions.rename(columns={'YEAR':'Year'}, inplace=True)
+
+mergedUSA=pd.merge(merged0,prescriptions,on=['FIPS','Year'],how='left',validate="m:m", indicator=True)
 
 mergedU=pd.merge(merged0,prescriptions,on='FIPS',how='left',validate="m:m", indicator=True)
 
 mergedU._merge.value_counts()
 
-#25 rows have no prescripts, in DC and Indiana. For now, I will drop them.
-mergedU.drop(mergedU[mergedU['_merge']=='left_only'].index,inplace=True)
+#Looks good.
 
 #dropmerge column and set up Florida 
 mergedU = mergedU.drop(columns = ['_merge']) 
@@ -57,50 +59,53 @@ mergedWG['Year']=mergedWG.index
 mergedUG=mergedU.groupby(['Year']).mean()
 mergedUG['Year']=mergedUG.index
 
-#Policy Years
+#Policy Years MergedXG is grouped data for visualizations
 mergedWG['Policy']=0
 mergedWG.Policy[mergedWG['Year']>=2010]=1
+mergedUG['Policy']=0
+mergedUG.Policy[mergedUG['Year']>=2010]=1
+mergedW['Policy']=0
+mergedW.Policy[mergedW['Year']>=2010]=1
+mergedU['Policy']=0
+mergedU.Policy[mergedU['Year']>=2010]=1
+
 
 #Initial Comparison
-p0=(ggplot(mergedWG, aes(x='Year', y='DeathsPC')) +
-        geom_point() +
+p0=(ggplot(mergedW, aes(x='Year', y='DeathsPC')) +
+        geom_point(alpha=0.01) +
         geom_smooth(method='lm',color='green') +
         geom_vline(xintercept=2009.5)
   )
 
 p0
-## Seems there might be something, lets do PrePost
+## Not sure of anything yet
 
 #Policy years comparison aka PrePost
-PrePost=(ggplot(mergedWG, aes(x='Year', y='DeathsPC')) +
-        geom_point() +
-        geom_smooth(data=mergedWG[mergedWG['Policy']==0], method='lm',color='black') +
-        geom_smooth(data=mergedWG[mergedWG['Policy']==1], method='lm',color='blue') +
+PrePost=(ggplot(mergedW, aes(x='Year', y='DeathsPC')) +
+        geom_point(alpha=0.006) +
+        geom_smooth(data=mergedW[mergedW['Policy']==0], method='lm',color='black') +
+        geom_smooth(data=mergedW[mergedW['Policy']==1], method='lm',color='blue') +
         geom_vline(xintercept=2009.5)
   )
 
 PrePost
 ## Seems like there is a change
 
-#Initial DIF DIF plot
-p2=(ggplot(mergedWG, aes(x='Year', y='DeathsPC')) +
-        geom_point() +
-       geom_point(data=mergedUG, color='red') +
-       geom_smooth(data=mergedUG, method='lm',color='red') +
-        geom_vline(xintercept=2006.5)
-  )
+#Year from policy data
+mergedW['polyear']=mergedW['Year']-2010
+mergedU['polyear']=mergedU['Year']-2010
+mergedUG['polyear']=mergedUG['Year']-2010
+mergedWG['polyear']=mergedWG['Year']-2010
 
-p2
-##Looks like USA has a clear upward trend, but Florida isnt following
 
 # Without overall
-DifDif2=(ggplot(mergedWG, aes(x='Year', y='DeathsPC')) +
-        geom_point() +
-       geom_point(data=mergedUG, color='red') +
-       geom_smooth(data=mergedUG, method='lm',color='red') +
-        geom_smooth(data=mergedWG[mergedWG['Policy']==0], method='lm',color='black', se =False) +
-        geom_smooth(data=mergedWG[mergedWG['Policy']==1], method='lm',color='blue',se=False) +
-        geom_vline(xintercept=2009.5)
+DifDif2=(ggplot(mergedW, aes(x='polyear', y='DeathsPC')) +
+         geom_point(data=mergedWG,color='darkblue',alpha=0.5) +
+         geom_point(data=mergedUG,color='darkred',alpha=0.5)+
+        geom_smooth(data=mergedW[mergedW['Policy']==0], method='lm',color='blue') +
+        geom_smooth(data=mergedW[mergedW['Policy']==1], method='lm',color='darkblue') +
+          geom_smooth(data=mergedU[mergedU['Policy']==0], method='lm',color='red') +
+        geom_smooth(data=mergedU[mergedU['Policy']==1], method='lm',color='darkred') 
   )
 
 DifDif2
@@ -149,6 +154,8 @@ mergedUG['Year']=mergedUG.index
 #Policy Years
 mergedWG['Policy']=0
 mergedWG.Policy[mergedWG['Year']>=2010]=1
+mergedUG['Policy']=0
+mergedUG.Policy[mergedUG['Year']>=2010]=1
 
 #Initial Comparison
 p0=(ggplot(mergedWG, aes(x='Year', y='PrescribePC')) +
@@ -161,10 +168,10 @@ p0
 ## Seems like there was no change
 
 #Policy years comparison aka PrePost
-PrePost=(ggplot(mergedWG, aes(x='Year', y='PrescribePC')) +
-        geom_point() +
-        geom_smooth(data=mergedWG[mergedWG['Policy']==0], method='lm',color='black') +
-        geom_smooth(data=mergedWG[mergedWG['Policy']==1], method='lm',color='blue') +
+PrePost=(ggplot(mergedW, aes(x='Year', y='PrescribePC')) +
+        geom_point(alpha=0.005) +
+        geom_smooth(data=mergedW[mergedW['Policy']==0], method='lm',color='black') +
+        geom_smooth(data=mergedW[mergedW['Policy']==1], method='lm',color='blue') +
         geom_vline(xintercept=2009.5)
   )
 
@@ -184,10 +191,11 @@ p2
 # Without overall
 DifDif2=(ggplot(mergedWG, aes(x='Year', y='PrescribePC')) +
         geom_point() +
-       geom_point(data=mergedUG, color='red') +
-       geom_smooth(data=mergedUG, method='lm',color='red') +
+           geom_point(data=mergedUG, color='red') +
         geom_smooth(data=mergedWG[mergedWG['Policy']==0], method='lm',color='black', se =False) +
-        geom_smooth(data=mergedWG[mergedWG['Policy']==1], method='lm',color='blue',se=False) +
+        geom_smooth(data=mergedWG[mergedWG['Policy']==1], method='lm',color='blue',se=False)  +
+          geom_smooth(data=mergedUG[mergedUG['Policy']==0], method='lm',color='red', se=False) +
+        geom_smooth(data=mergedUG[mergedUG['Policy']==1], method='lm',color='darkred', se=False) +
         geom_vline(xintercept=2009.5)
   )
 
@@ -195,3 +203,4 @@ DifDif2
 
 PrePost.save("../../30_results/Graphs/FloridaPRESCRIBEPrePost.png")
 DifDif2.save("../../30_results/Graphs/FloridaPRESCRIBEDifDif.png")
+
